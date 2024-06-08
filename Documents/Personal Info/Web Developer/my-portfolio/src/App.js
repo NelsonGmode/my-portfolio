7d1dd146912fb46587a04cb12e9afcfb086ea54d
Nelson Gmode <nelsongmode@gmail.com>
2024-06-08 17:20:55 +0800

    Updated React Router to use Routes and Route

--- Documents/Personal Info/Web Developer/my-portfolio/src/App.js
+++ Documents/Personal Info/Web Developer/my-portfolio/src/App.js
@@ -1,18 +1,36 @@
-// src/App.js
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './Home';
 import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to My Portfolio</h1>
-      </header>
-      <About />
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About Me</Link></li>
+              <li><Link to="/projects">Projects</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
---
+++ Documents/Personal Info/Web Developer/my-portfolio/src/Projects.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div>
+      <h2>My Projects</h2>
+      <p>Here are some of the projects I've worked on.</p>
+      <ul>
+        <li>Project One</li>
+        <li>Project Two</li>
+        <li>Project Three</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Projects;
